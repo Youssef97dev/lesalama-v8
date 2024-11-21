625b9eb4f5b65d58bce2e1c5f9334d98ea0f1c73
Youssef97dev <youssefmoussafi97@gmail.com>
2024-11-21 11:15:14 +0100

    change map

--- src/components/Mapbox.jsx
+++ src/components/Mapbox.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Mapbox = () => {
   return (
-    <div className="w-full ">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3397.2810280229282!2d-7.985970000000001!3d31.626155000000004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdafee422b547dcb%3A0x77d12fb6b8ea2d42!2sLe%20Salama!5e0!3m2!1sfr!2sma!4v1727864619629!5m2!1sfr!2sma"
-        height="500"
-        style={{ border: 0, width: "100%" }}
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+    <div className="flex justify-center items-center w-full">
+      <Link
+        href="https://maps.app.goo.gl/Zq999WxGBS6JFBYc8"
+        target="_blank"
+        className="w-1/2"
+      >
+        <Image
+          src="https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/Salama/qkl3mjzi6gbwzvfdt057"
+          alt="le salama map"
+          width={1000}
+          height={500}
+          className=" w-full object-cover"
+        />
+      </Link>
     </div>
   );
 };
